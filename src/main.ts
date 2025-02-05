2c8f66b4b212f53eb8afcff1318d45eb7abfdf3b
FrancoStoll <francostoll@gmail.com>
2025-02-05 17:35:27 -0300

    Add console log

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,8 @@ async function bootstrap() {
 
   await app.listen(envs.port ?? 3000);
 
+  console.log('Hola mundo - Primer cambio')
+
 
   logger.log(`Gateway is running on ${envs.port ?? 3000} port`);
 }

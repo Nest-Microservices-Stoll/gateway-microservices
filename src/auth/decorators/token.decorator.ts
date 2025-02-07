525e5c7f7024b12bd6d4f55a6f3af6f9ed702171
FrancoStoll <francostoll@gmail.com>
2025-02-07 18:58:12 -0300

    Add authentication module with user registration and login functionality

---
+++ src/auth/decorators/token.decorator.ts
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+import { createParamDecorator, ExecutionContext, InternalServerErrorException } from '@nestjs/common';
+
+
+export const Token = createParamDecorator(
+
+    (data: unknown, ctx: ExecutionContext) => {
+        const request = ctx.switchToHttp().getRequest();
+
+        if (!request.token) throw new InternalServerErrorException('Token not found in request (AuthGuardCalled?)');
+
+
+
+
+        return request.token;
+
+
+    },
+
+
+);
---
+++ src/auth/dto/login-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsString } from "class-validator";
+
+
+export class LoginUserDto {
+
+
+
+    @IsString()
+    @IsEmail()
+    email: string;
+
+
+    @IsString()
+    password: string
+
+
+}
